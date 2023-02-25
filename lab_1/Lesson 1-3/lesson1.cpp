f346bec3bd6757191f79155739e6af036c80f1e0
Ivan <vany.kozlov5@mail.ru>
2023-02-25 14:07:55 +0500

    final

--- lab_1/Lesson 1-3/lesson1.cpp
+++ lab_1/Lesson 1-3/lesson1.cpp
@@ -6,17 +6,18 @@
 GLuint VBO; //указатель на буфер
 
 void RenderSceneCB() {
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT); //очистка буфера кадра
 
-    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(0); //включаем атрибут вершины
+
     glBindBuffer(GL_ARRAY_BUFFER, VBO); //привязываем указатель 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawArrays(GL_TRIANGLES, 0, 3); //функция для отрисовки
 
-    glDisableVertexAttribArray(0);
+    glDisableVertexAttribArray(0);  
 
-    glutSwapBuffers();
+    glutSwapBuffers(); //меняет буфер кадра и фоновый буфер местами
 }
 
 
@@ -28,31 +29,31 @@ void CreateVertexBuffer()
     Vertices[2] = Vector3f(0.0f, 1.0f, 0.0f);
 
     glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO); //указываем что будут хранится вершины в буфере
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW); //связывание с данными
 }
 
 int main(int argc, char** argv) {
     glutInit(&argc, argv);  //инициализация glut
 
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA); //GLUT_DOUBLE включает двойную буферизацию (отрисовка будет происходить в фоновый буфер, в то время как другой буфер отображается)
 
     glutInitWindowSize(1024, 768);
     glutInitWindowPosition(100, 100);
     glutCreateWindow("Tutorial 01");
 
-    glutDisplayFunc(RenderSceneCB);
+    glutDisplayFunc(RenderSceneCB); //функция обратного вызова
 
 
-    GLenum res = glewInit();
+    GLenum res = glewInit(); //инициализируем glew
     if (res != GLEW_OK) {
         fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
         return 1;
     }
 
-    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f); //цвет, который будет использован во время очистки буфера кадра
 
     CreateVertexBuffer();
 
-    glutMainLoop();
+    glutMainLoop(); //передаёт управление основному циклу glut
 }
