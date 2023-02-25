58e53c9bbadc8be4d9cad7621667fb0362f2b3df
Ivan <vany.kozlov5@mail.ru>
2023-02-25 13:08:28 +0500

    1st lab
    1st lab

---
+++ lab_1/Lesson 1-3/lesson1.cpp
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <glew.h>
+#include <glut.h>
+#include "math_3d.h"
+
+GLuint VBO; //указатель на буфер
+
+void RenderSceneCB() {
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO); //привязываем указатель 
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    glDisableVertexAttribArray(0);
+
+    glutSwapBuffers();
+}
+
+
+void CreateVertexBuffer()
+{
+    Vector3f Vertices[3];
+    Vertices[0] = Vector3f(-1.0f, -1.0f, 0.0f);
+    Vertices[1] = Vector3f(1.0f, -1.0f, 0.0f);
+    Vertices[2] = Vector3f(0.0f, 1.0f, 0.0f);
+
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);  //инициализация glut
+
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+
+    glutInitWindowSize(1024, 768);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Tutorial 01");
+
+    glutDisplayFunc(RenderSceneCB);
+
+
+    GLenum res = glewInit();
+    if (res != GLEW_OK) {
+        fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
+        return 1;
+    }
+
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+    CreateVertexBuffer();
+
+    glutMainLoop();
+}
